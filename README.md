a20777abe1b4e540ea803dcfc70a48e68aa613ab
Mario Ryzen <mario.calvo@outlook.com>
2019-07-20 10:11:27 -0600

    configuracion inicial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-avanzado
+# curso-platzi-react-avanzado ⚛️
+
+Repositorio con el código del [Curso Avanzado de React de Platzi](https://platzi.com/cursos/react-avanzado/)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const HolaMundo = () => {
+  return <h1>Hola Mundo</h1>
+}
+
+ReactDOM.render(<HolaMundo />, document.getElementById('app'))
