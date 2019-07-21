30c8f5b31a39fd98eaa9673e472049c091e15606
Mario Hugo (Lenovo) <mario.calvo@outlook.com>
2019-07-20 18:25:28 -0600

    PhotoCard y ListOfPhotoCards (primera etapa)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ListOfCategories } from './components/listOfCategories'
+import { ListOfPhotoCards } from './components/listOfPhotoCards'
 import { GlobalStyle } from './GlobalStyles'
 export const App = () => (
   <React.Fragment>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </React.Fragment>
 )
