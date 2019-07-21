95cce53d4a11e55fcaa5e2c7290cd8130ae4cfef
Mario Ryzen <mario.calvo@outlook.com>
2019-07-21 09:21:15 -0600

    Logo component and global animation styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/listOfCategories'
 import { ListOfPhotoCards } from './components/listOfPhotoCards'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
 export const App = () => (
   <React.Fragment>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </React.Fragment>
